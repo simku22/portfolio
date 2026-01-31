5c91bc3a5af7fc4abf557bc4da99ab0960330b2d
Simon Kurgan <simku22@uw.edu>
2026-01-30 22:48:19 -0800

    added eslint/prettier

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  // Ignore patterns
+  {
+    ignores: ['dist/', '.astro/', 'node_modules/', '*.config.mjs'],
+  },
+  // TypeScript files
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  // Astro files
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.astro'],
+    rules: {
+      // Add any custom Astro rules here
+    },
+  },
+];
